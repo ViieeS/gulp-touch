e9376fb2fca355d1377384da551a07cd4062a140
ViieeS <vies@list.ru>
2017-03-13 17:26:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var fs = require('fs');
+var through = require('through2');
+
+module.exports = function () {
+	return through.obj(function (file, enc, cb) {
+		var current = new Date();
+		this.push(file);
+		fs.utimes(file.path, current, current, cb);
+	});
+};
